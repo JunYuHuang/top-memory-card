fb3b6cee9ea85a722f97561920ae93158cf98782
Jun Huang <1624698+JunYuHuang@users.noreply.github.com>
2023-12-13 12:23:15 -0800

    Add dummyCards.js

---
+++ src/lib/dummyCards.js
@@ -0,0 +1,62 @@
+export const dummyCards = [
+  {
+    id: "0",
+    title: "Dallas",
+    imgSrc: "DallasPD2.jpg",
+  },
+  {
+    id: "1",
+    title: "Wolf",
+    imgSrc: "WolfPD2.jpg",
+  },
+  {
+    id: "2",
+    title: "Chains",
+    imgSrc: "ChainsPD2.jpg",
+  },
+  {
+    id: "3",
+    title: "Hoxton",
+    imgSrc: "HoxtonPD2.jpg",
+  },
+  {
+    id: "4",
+    title: "Houston",
+    imgSrc: "HoustonPD2.jpg",
+  },
+  {
+    id: "5",
+    title: "Jacket",
+    imgSrc: "JacketPD2.jpg",
+  },
+  {
+    id: "6",
+    title: "Bodhi",
+    imgSrc: "BodhiPD2.png",
+  },
+  {
+    id: "7",
+    title: "Duke",
+    imgSrc: "DukePD2.jpg",
+  },
+  {
+    id: "8",
+    title: "Jimmy",
+    imgSrc: "JimmyPD2.jpg",
+  },
+  {
+    id: "9",
+    title: "Joy",
+    imgSrc: "JoyPD2.png",
+  },
+  {
+    id: "10",
+    title: "Sangres",
+    imgSrc: "SangresPD2.jpg",
+  },
+  {
+    id: "11",
+    title: "Bonnie",
+    imgSrc: "BonniePD2.jpg",
+  },
+];
